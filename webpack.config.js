7f797f58274b2877bdaf8aa426af309001297c34
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-24 15:33:25 -0700

    codesplit the app into separate concers

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack  = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './assets/js/script.js',
@@ -12,6 +13,9 @@ module.exports = {
       $: "jquery",
       jQuery: "jquery"
     }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
   ],
   mode: 'development'
 };
