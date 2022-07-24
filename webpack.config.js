9b1b1900393b1cc425d4eca190f97c9b7589aee6
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-24 14:12:19 -0700

    19.2.4

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: './assets/js/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  mode: 'development'
+};
