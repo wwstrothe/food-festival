645a2f8bed339fe0048764988f1e13befb786549
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-24 14:23:53 -0700

    19.2.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack  = require("webpack");
 
 module.exports = {
   entry: './assets/js/script.js',
@@ -6,5 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+  ],
   mode: 'development'
 };
