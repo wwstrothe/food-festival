c55ffc5abe08447647a425436271fe6fd94ff08d
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-24 18:43:09 -0700

    added pwa

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack  = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const { config } = require("process");
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -45,6 +46,21 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "disabled", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons')
+      }]
     })
   ],
   mode: 'development'
