5d9ba857b0fedca140aa426288ea2150277d33a1
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-24 18:18:02 -0700

    created service workers for offline functionality

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,5 +1,6 @@
 require("bootstrap");
 const createEl = require("./domMethods");
+const { createLoremIpsum, dateConverter } = require("./helpers");
 
 $(document).ready(function () {
   if (window.location.href.indexOf("schedule") > -1) {
